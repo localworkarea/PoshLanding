b4b2da47898217fe5ec85cc707d2b595d11706fd
localworkarea <localworkarea@gmail.com>
2025-11-10 16:31:40 +0100

    up

--- src/components/layout/slider/slider.js
+++ src/components/layout/slider/slider.js
@@ -1,43 +1,26 @@
-/*
-Документація по роботі у шаблоні: 
-Документація слайдера: https://swiperjs.com/
-Сніппет(HTML): swiper
-*/
-
-// Підключаємо слайдер Swiper з node_modules
-// При необхідності підключаємо додаткові модулі слайдера, вказуючи їх у {} через кому
-// Приклад: { Navigation, Autoplay }
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 /*
 Основні модулі слайдера:
 Navigation, Pagination, Autoplay, 
 EffectFade, Lazy, Manipulation
-Детальніше дивись https://swiperjs.com/
 */
 
-// Стилі Swiper
 // Підключення базових стилів
 import "./slider.scss";
 // Повний набір стилів з node_modules
 // import 'swiper/css/bundle';
 
-// Ініціалізація слайдерів
 function initSliders() {
-	// Список слайдерів
-	// Перевіряємо, чи є слайдер на сторінці
-	if (document.querySelector('.swiper')) { // <- Вказуємо склас потрібного слайдера
-		// Створюємо слайдер
-		new Swiper('.swiper', { // <- Вказуємо склас потрібного слайдера
-			// Підключаємо модулі слайдера
-			// для конкретного випадку
-			modules: [Navigation],
+	if (document.querySelector('.cost-guarantee__slider')) {
+		new Swiper('.cost-guarantee__slider', { 
+			// modules: [],
 			observer: true,
 			observeParents: true,
-			slidesPerView: 1,
+			slidesPerView: 3,
 			spaceBetween: 0,
 			//autoHeight: true,
-			speed: 800,
+			speed: 500,
 
 			//touchRatio: 0,
 			//simulateTouch: false,
@@ -45,64 +28,137 @@ function initSliders() {
 			//preloadImages: false,
 			//lazy: true,
 
-			/*
-			// Ефекти
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
-
-			// Пагінація
-			/*
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			*/
-
-			// Скроллбар
-			/*
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			},
-			*/
-
-			// Кнопки "вліво/вправо"
-			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
-			},
-			/*
-			// Брейкпоінти
+		
 			breakpoints: {
-				640: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
+				320: {
+					slidesPerView: 1.1,
+					spaceBetween: 12,
 				},
 				768: {
-					slidesPerView: 2,
+					slidesPerView: 2.1,
 					spaceBetween: 20,
 				},
-				992: {
+				1024: {
 					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
+					spaceBetween: 32,
 				},
 			},
-			*/
 			// Події
 			on: {
 
 			}
 		});
 	}
+	if (document.querySelector('.reviews__slider')) {
+		new Swiper('.reviews__slider', { 
+			// modules: [],
+			observer: true,
+			observeParents: true,
+			spaceBetween: 0,
+			//autoHeight: true,
+			speed: 500,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+		
+			breakpoints: {
+				320: {
+					slidesPerView: 1.1,
+					spaceBetween: 12,
+				},
+				768: {
+					slidesPerView: 1.2,
+					spaceBetween: 20,
+				},
+				1024: {
+					slidesPerView: 1.52,
+					spaceBetween: 45,
+				},
+			},
+			// Події
+			on: {
+
+			}
+		});
+	}
+	// if (document.querySelector('.swiper')) {
+	// 	new Swiper('.swiper', { 
+	// 		modules: [Navigation],
+	// 		observer: true,
+	// 		observeParents: true,
+	// 		slidesPerView: 1,
+	// 		spaceBetween: 0,
+	// 		//autoHeight: true,
+	// 		speed: 800,
+
+	// 		//touchRatio: 0,
+	// 		//simulateTouch: false,
+	// 		//loop: true,
+	// 		//preloadImages: false,
+	// 		//lazy: true,
+
+	// 		/*
+	// 		// Ефекти
+	// 		effect: 'fade',
+	// 		autoplay: {
+	// 			delay: 3000,
+	// 			disableOnInteraction: false,
+	// 		},
+	// 		*/
+
+	// 		// Пагінація
+	// 		/*
+	// 		pagination: {
+	// 			el: '.swiper-pagination',
+	// 			clickable: true,
+	// 		},
+	// 		*/
+
+	// 		// Скроллбар
+	// 		/*
+	// 		scrollbar: {
+	// 			el: '.swiper-scrollbar',
+	// 			draggable: true,
+	// 		},
+	// 		*/
+
+	// 		// Кнопки "вліво/вправо"
+	// 		navigation: {
+	// 			prevEl: '.swiper-button-prev',
+	// 			nextEl: '.swiper-button-next',
+	// 		},
+	// 		/*
+	// 		// Брейкпоінти
+	// 		breakpoints: {
+	// 			640: {
+	// 				slidesPerView: 1,
+	// 				spaceBetween: 0,
+	// 				autoHeight: true,
+	// 			},
+	// 			768: {
+	// 				slidesPerView: 2,
+	// 				spaceBetween: 20,
+	// 			},
+	// 			992: {
+	// 				slidesPerView: 3,
+	// 				spaceBetween: 20,
+	// 			},
+	// 			1268: {
+	// 				slidesPerView: 4,
+	// 				spaceBetween: 30,
+	// 			},
+	// 		},
+	// 		*/
+	// 		// Події
+	// 		on: {
+
+	// 		}
+	// 	});
+	// }
 }
 document.querySelector('[data-fls-slider]') ?
 	window.addEventListener("load", initSliders) : null
